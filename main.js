d9ce4aaee4d47a83e016df6395aa0ab82d527a55
shinonetae <shinonetae@gmail.com>
2026-06-12 09:39:35 +0000

    Add dark/light mode functionality

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-// Add JS here
+const themeToggle = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'light';
+
+if (currentTheme === 'dark') {
+  document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+themeToggle.addEventListener('click', () => {
+  let theme = document.documentElement.getAttribute('data-theme');
+  if (theme === 'dark') {
+    document.documentElement.removeAttribute('data-theme');
+    localStorage.setItem('theme', 'light');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+});
